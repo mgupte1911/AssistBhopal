6e5066fc7e409ec065b7fc53b85c9d2131d7d08c
monyagupte <monish.gupte@mservices.in>
2018-12-04 01:12:58 +0530

    Video uploaded and Message Send updated
    Video uploaded with mp4 format and Send Message section completed.

--- Assist Bhopal Project/Assist Bhopal Website/send_message.php
+++ Assist Bhopal Project/Assist Bhopal Website/send_message.php
@@ -1,16 +1,19 @@
 <?php
-	if(isset($_POST['submit']))
-  {
+	if(isset($_POST['submit'])){
 		$name=$_POST['name'];
 		$email=$_POST['email'];
-		$message=$_POST['message'];
+		$msg=$_POST['msg'];
 
-		$to='monyagupte@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Assist Bhopal Message';
-		$message="Name :".$name."\n"."Wrote the following :"."\n\n".$message;
+		$to='assistbhopal@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Assist Bhopal New Message';
+		$message="Name :".$name."\n"."Wrote the following message:"."\n\n".$msg;
 		$headers="From: ".$email;
 
-		mail($to, $subject, $message, $headers);
-   
+		if(mail($to, $subject, $message, $headers)){
+			 echo '';
+		}
+		else{
+			echo "Something went wrong!";
+		}
 	}
 ?>
